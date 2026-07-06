4f8cf93beff6233f45396020932e1b1580560416
Aishwarya Batchu <aishwaryabatchu63@gmail.com>
2026-07-06 14:27:37 +0530

    updated deployment files (procfile, requirements)

--- app.py
+++ app.py
@@ -72,5 +72,8 @@ def predict():
 
         return render_template('index.html', prediction=crop_name.upper())
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
